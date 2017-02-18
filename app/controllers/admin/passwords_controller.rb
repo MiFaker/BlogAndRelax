81d926a972665352607c719f94e5b31f62d2f207
MinhDT <minhdt.cntt@gmail.com>
2017-02-18 18:07:59 +0700

    Send-Mail-Reset-Password

---
+++ app/controllers/admin/passwords_controller.rb
@@ -0,0 +1,23 @@
+class Admin::PasswordsController < Devise::PasswordsController
+  def new
+    self.resource = resource_class.new
+  end
+
+  def create
+    self.resource = resource_class
+      .send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+
+    if resource.errors.present?
+      respond_with(resource)
+    else
+      respond_with({}, location: after_send_reset_password_url)
+      set_flash_message! :notice, :send_instructions
+    end
+  end
+
+  protected
+  def after_send_reset_password_url
+    new_admin_session_path if is_navigational_format?
+  end
+end
